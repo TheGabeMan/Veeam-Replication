a2fac7fa8a3f3338e0c26ca72ec9c35ec57b286e
Gabrie van Zanten <thegabeman@gmail.com>
2017-01-18 14:30:51 +0100

    Create PermanentFailOverVM.ps1

---
+++ PermanentFailOverVM.ps1
@@ -0,0 +1,11 @@
+# Snapin nodig? 
+# Add-PSSnapin VeeamPSSnapin
+
+
+$VMFailover = read-host "Welke VM PERMANENT overfailen?"
+$VMFound = Get-VBRRestorePoint -Name $VMFailover | Sort-Object $_.creationtime -Descending | Select -First 1
+$Continue = Read-host "(YES/NO) Continue with " $vmfound.vmname " Last sync: " $vmfound.CreationTime
+If( $Continue = "YES")
+{
+	Get-VBRRestorePoint -Name $VMFailover | Sort-Object $_.creationtime -Descending | Select -First 1 | Start-VBRViReplicaFailover -Reason "Batch 2 (14jan)" -RunAsync -Definite -Confirm
+}
