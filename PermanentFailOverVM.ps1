93e5a4936187f9b8bf1965e66018e1c1f68f496e
Gabrie van Zanten <thegabeman@gmail.com>
2017-01-18 14:50:18 +0100

    Update PermanentFailOverVM.ps1

--- PermanentFailOverVM.ps1
+++ PermanentFailOverVM.ps1
@@ -1,8 +1,5 @@
-# Snapin nodig? 
 # Add-PSSnapin VeeamPSSnapin
-
-
-$VMFailover = read-host "Welke VM PERMANENT overfailen?"
+$VMFailover = read-host "Which VM to PERMANENT failover?"
 $VMFound = Get-VBRRestorePoint -Name $VMFailover | Sort-Object $_.creationtime -Descending | Select -First 1
 $Continue = Read-host "(YES/NO) Continue with " $vmfound.vmname " Last sync: " $vmfound.CreationTime
 If( $Continue = "YES")
