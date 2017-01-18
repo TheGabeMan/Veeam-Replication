cd7333236ad4f468af8f06148b14003e8f4a061b
Gabrie van Zanten <thegabeman@gmail.com>
2017-01-18 14:46:50 +0100

    Update CreateReplicationJobs.ps1

--- CreateReplicationJobs.ps1
+++ CreateReplicationJobs.ps1
@@ -1,13 +1,13 @@
 # Reading a CSV with VMs that needs to be replicated
 # List contains VMNaam, vCenterNaam (vCenter as described in VEEAM)
 # Authors: Annelies Maex & Gabrie van Zanten & Joren De Spaey
-# linkedin: https://be.linkedin.com/in/anneliesmaex
-# linkedin: https://www.linkedin.com/in/jorendespaey
+# Annelies linkedin: https://be.linkedin.com/in/anneliesmaex
+# Joren linkedin: https://www.linkedin.com/in/jorendespaey
+# Gabrie van Zanten: http://www.GabesVirtualWorld.com
 
 #Add-PSSnapin VeeamPSSnapin
-# $VMRepList = Get-Content "vmList-vCenter01.txt"
+$VMRepList = Get-Content "vmList-vCenter01.txt"
 # $VMRepList = Get-Content "vmList-vCenter02.txt"
-$VMRepList = Get-Content "vmList-vCenter03.txt"
 
 # Replication Job settings:
 # https://helpcenter.veeam.com/docs/backup/powershell/add-vbrvireplicajob.html?ver=95
@@ -29,21 +29,17 @@ $Suffix = " "
 # Target PortGroup = TargetPortGroup
 
 # Source ESXi host = SourceESXi
-# Source ESXi voor vCenter1
-# $SourceESXi = "esxhtw05a.opg.local"
+# Source ESXi for vCenter1
+$SourceESXi = "esx001.source.local"
 
-# Source ESXi voor vCenter2
-# $SourceESXi = "esxtys028.opg.local" 
-
-# Source ESXi voor vCenter3
-$SourceESXi = "esxhtw021.opg.local"
+# Source ESXi for vCenter2
+# $SourceESXi = "esx080.source.local" 
 
 # Target ESXi host = TargetESXi
-$TargetESXi = "mdqvm001.mdq-openline.local"
+$TargetESXi = "esx-aaa.target.local"
 
-# $NetworkMappingCSV = Import-Csv "VLAN-vCenter01.csv"
+$NetworkMappingCSV = Import-Csv "VLAN-vCenter01.csv"
 # $NetworkMappingCSV = Import-Csv "VLAN-vCenter02.csv"
-$NetworkMappingCSV = Import-Csv "VLAN-vCenter03.csv"
 
 $NetworkMappingList = @()
 
@@ -57,15 +53,15 @@ ForEach( $Mapping in $NetworkMappingCSV)
 
 # Source Proxy: Specifies the source proxy you want to assign to the job. Default: automatic selection.
 # Target Proxy: Specifies the target proxy you want to assign to the job. Default: automatic selection.
-# Retreive proxies using: get-vbrviproxy -name "veeamproxy1"
+# Retreive proxies using: get-vbrviproxy -name "sourceproxy1"
 
 $ObjSourceProxy = @()
-$ObjSourceProxy += Get-VBRViProxy -name "veeamproxy1"
-$ObjSourceProxy += Get-VBRViProxy -name "veeamproxy2"
+$ObjSourceProxy += Get-VBRViProxy -name "sourceproxy1"
+$ObjSourceProxy += Get-VBRViProxy -name "sourceproxy2"
 
 $ObjTargetProxy = @()
-$ObjTargetProxy += Get-VBRViProxy -name "MDQPR003"
-$ObjTargetProxy += Get-VBRViProxy -name "MDQPR004"
+$ObjTargetProxy += Get-VBRViProxy -name "targetproxy1"
+$ObjTargetProxy += Get-VBRViProxy -name "targetproxy2"
 
 # Restore Points ToKeep: Specifies the number of restore points you want to keep. Permitted values: 1 to 28. Default: 7.
 $RestorePoints = 1
@@ -75,46 +71,47 @@ $RestorePoints = 1
 
 
 # Per VM we will create a new replication job
+# To prevent bombing the vCenter and ESXi hosts, each schedule will start a minute after the previous
 $10minutes = 0
 $minutes = 0
 
 ForEach( $ReplVM in $VMRepList)
 {	
 	# Get Source vCenter name in VEEAM Server
-	$SourcevCenter = Get-VBRServer -Name "vcentglp01.opg.local"
-    # $SourcevCenter = Get-VBRServer -Name "vcentglp02.opg.local"
-    $TargetvCenter = Get-VBRServer -Name "MDQVC001"
+	$SourcevCenter = Get-VBRServer -Name "vcenter01.source.local"
+    	# $SourcevCenter = Get-VBRServer -Name "vcenter02.source.local"
+    	$TargetvCenter = Get-VBRServer -Name "vcenter.target.local"
 
 	# Search VM to be replicated in Source vCenter
 	$EntSourceObj = @()
 	$EntSourceObj = Find-VBRViEntity -Server $SourcevCenter -Name $ReplVM
 
-    $EntTargetObj = @()
+    	$EntTargetObj = @()
 	$EntTargetObj = Find-VBRViEntity -Server $TargetvCenter -Name $ReplVM
 	
 	# Step 1: Create the job (name, server, suffix, entity, networkmapping, proxy, restore points)
-    # Read-Host $ReplVM 
-	Add-VBRViReplicaJob -Name "Batch_3_vcentglp01_$ReplVM" -Server $TargetESXi -Suffix $Suffix -Entity $EntSourceObj -EnableNetworkMapping -SourceNetwork $NetworkMappingList.Source -TargetNetwork $NetworkMappingList.Target -SourceProxy $ObjSourceProxy -TargetProxy $ObjTargetProxy -RestorePointsToKeep $RestorePoints -OriginalVM $EntSourceObj -ReplicaVM $EntTargetObj
+	Add-VBRViReplicaJob -Name "Batch_1_$ReplVM" -Server $TargetESXi -Suffix $Suffix -Entity $EntSourceObj -EnableNetworkMapping -SourceNetwork $NetworkMappingList.Source -TargetNetwork $NetworkMappingList.Target -SourceProxy $ObjSourceProxy -TargetProxy $ObjTargetProxy -RestorePointsToKeep $RestorePoints -OriginalVM $EntSourceObj -ReplicaVM $EntTargetObj
     
-    # Step 2: Modify settings of the job (Enable replica seeding + VMMapping)
-    $job = Get-VBRJob -Name "Batch_3_vcentglp01_$ReplVM" 
-    Set-VBRViReplicaJob -Job $job -EnableVMMapping  
+    	# Step 2: Modify settings of the job (Enable replica seeding + VMMapping)
+    	$job = Get-VBRJob -Name "Batch_1_$ReplVM" 
+    	Set-VBRViReplicaJob -Job $job -EnableVMMapping  
 
-    # Set and enable the Replication Schedule
-    # Start time HOUR then there will be an automatic addition of 1 minute to the start time to prevent bombing the vCenter
-    $time = "10:$10minutes$minutes"
-    Set-VBRJobSchedule -Job $job -Daily -At $time
-    Enable-VBRJobSchedule -Job $job
+	# Set and enable the Replication Schedule
+    	# Start time HOUR then there will be an automatic addition of 1 minute to the start time to prevent bombing the vCenter
+    	$time = "10:$10minutes$minutes"
+    	Set-VBRJobSchedule -Job $job -Daily -At $time
+    	Enable-VBRJobSchedule -Job $job
 
-    #Enable Backup Window
-    $ScheduleOptions = Get-VBRJobScheduleOptions -Job $job
-    $ScheduleOptions.OptionsBackupWindow.IsEnabled = $true
-    $ScheduleOptions.OptionsBackupWindow.BackupWindow = "<scheduler><Sunday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Sunday><Monday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Monday><Tuesday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Tuesday><Wednesday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Wednesday><Thursday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Thursday><Friday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Friday><Saturday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Saturday></scheduler>"
-    Set-VBRJobScheduleOptions -job $job -Options $ScheduleOptions
-    if ($minutes -lt 9){
-        $minutes += 1
-    } else {
-        $minutes = 0
-        $10minutes += 1
-    }
-}
+    	#Enable Backup Window
+    	$ScheduleOptions = Get-VBRJobScheduleOptions -Job $job
+    	$ScheduleOptions.OptionsBackupWindow.IsEnabled = $true
+	# Window allow to run is daily from 10am through 10pm
+    	$ScheduleOptions.OptionsBackupWindow.BackupWindow = "<scheduler><Sunday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Sunday><Monday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Monday><Tuesday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Tuesday><Wednesday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Wednesday><Thursday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Thursday><Friday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Friday><Saturday>1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1</Saturday></scheduler>"
+    	Set-VBRJobScheduleOptions -job $job -Options $ScheduleOptions
+    	if ($minutes -lt 9){
+        	$minutes += 1
+    	} else {
+        	$minutes = 0
+        	$10minutes += 1
+    	}
+   }
